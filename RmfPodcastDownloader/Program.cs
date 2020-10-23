39b87584493b91801451573495c400b9152bbe42
geralt18 <geralt18@gmail.com>
2020-10-23 09:53:21 +0200

    Add project files.

---
+++ RmfPodcastDownloader/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace RmfPodcastDownloader
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            //_rssUrl = "https://www.rmf.fm/rss/podcast/historia-dla-doroslych.xml";
+            //_rssUrl = "https://www.rmf.fm/rss/podcast/dorwac-bestie.xml";
+            _rssUrl = "https://www.rmf.fm/rss/podcast/sceny-zbrodni.xml";
+
+            try
+            {
+                string responseBody = await _client.GetStringAsync(_rssUrl);
+                XmlSerializer serializer = new XmlSerializer(typeof(rss));
+                rss podcasts;
+
+
+                using (StringReader stringReader = new StringReader(responseBody))
+                {
+                    // Call the Deserialize method to restore the object's state.
+                    podcasts = (rss)serializer.Deserialize(stringReader);
+                }
+
+                string path = Path.Combine(_baseDir, podcasts.channel.title);
+                if (!Directory.Exists(path))
+                    Directory.CreateDirectory(path);
+
+                foreach (var p in podcasts.channel.item)
+                {
+                    DateTime podcastDate = DateTime.Parse(p.pubDate);
+                    string fileName = CleanFilePath($"{podcastDate:yyyy-MM-dd} - {p.title}.mp3");
+                    string filePath = Path.Combine(path, fileName);
+                    if (File.Exists(filePath))
+                        continue;
+
+                    Console.WriteLine("Pobieram {0}", fileName);
+                    using (HttpResponseMessage response = await _client.GetAsync(p.enclosure.url, HttpCompletionOption.ResponseHeadersRead))
+                    using (Stream streamToReadFrom = await response.Content.ReadAsStreamAsync())
+                    {
+                        using (Stream streamToWriteTo = File.Open(filePath, FileMode.Create))
+                        {
+                            await streamToReadFrom.CopyToAsync(streamToWriteTo);
+                        }
+                    }
+                }
+
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("\nException Caught!");
+                Console.WriteLine("Message :{0} ", e.Message);
+            }
+        }
+
+        private static string CleanFilePath(string input)
+        {
+            string regexSearch = new string(Path.GetInvalidFileNameChars()) + new string(Path.GetInvalidPathChars());
+            Regex r = new Regex(string.Format("[{0}]", Regex.Escape(regexSearch)));
+            return r.Replace(input, "");
+        }
+
+        static readonly HttpClient _client = new HttpClient();
+        static string _rssUrl = "";
+        static string _baseDir = @"D:\Rmf";
+    }
+}
---
+++ RmfPodcastDownloader/Rss.cs
@@ -0,0 +1,499 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RmfPodcastDownloader
+{
+
+    // NOTE: Generated code may require at least .NET Framework 4.5 or .NET Core/Standard 2.0.
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    [System.Xml.Serialization.XmlRootAttribute(Namespace = "", IsNullable = false)]
+    public partial class rss
+    {
+
+        private rssChannel channelField;
+
+        private decimal versionField;
+
+        /// <remarks/>
+        public rssChannel channel {
+            get {
+                return this.channelField;
+            }
+            set {
+                this.channelField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public decimal version {
+            get {
+                return this.versionField;
+            }
+            set {
+                this.versionField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    public partial class rssChannel
+    {
+
+        private string titleField;
+
+        private string linkField;
+
+        private string descriptionField;
+
+        private rssChannelImage imageField;
+
+        private string languageField;
+
+        private string copyrightField;
+
+        private string lastBuildDateField;
+
+        private byte ttlField;
+
+        private string authorField;
+
+        private string explicitField;
+
+        private owner ownerField;
+
+        private image image1Field;
+
+        private category categoryField;
+
+        private rssChannelItem[] itemField;
+
+        /// <remarks/>
+        public string title {
+            get {
+                return this.titleField;
+            }
+            set {
+                this.titleField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string link {
+            get {
+                return this.linkField;
+            }
+            set {
+                this.linkField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string description {
+            get {
+                return this.descriptionField;
+            }
+            set {
+                this.descriptionField = value;
+            }
+        }
+
+        /// <remarks/>
+        public rssChannelImage image {
+            get {
+                return this.imageField;
+            }
+            set {
+                this.imageField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string language {
+            get {
+                return this.languageField;
+            }
+            set {
+                this.languageField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string copyright {
+            get {
+                return this.copyrightField;
+            }
+            set {
+                this.copyrightField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string lastBuildDate {
+            get {
+                return this.lastBuildDateField;
+            }
+            set {
+                this.lastBuildDateField = value;
+            }
+        }
+
+        /// <remarks/>
+        public byte ttl {
+            get {
+                return this.ttlField;
+            }
+            set {
+                this.ttlField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute(Namespace = "http://www.itunes.com/dtds/podcast-1.0.dtd")]
+        public string author {
+            get {
+                return this.authorField;
+            }
+            set {
+                this.authorField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute(Namespace = "http://www.itunes.com/dtds/podcast-1.0.dtd")]
+        public string @explicit {
+            get {
+                return this.explicitField;
+            }
+            set {
+                this.explicitField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute(Namespace = "http://www.itunes.com/dtds/podcast-1.0.dtd")]
+        public owner owner {
+            get {
+                return this.ownerField;
+            }
+            set {
+                this.ownerField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute("image", Namespace = "http://www.itunes.com/dtds/podcast-1.0.dtd")]
+        public image image1 {
+            get {
+                return this.image1Field;
+            }
+            set {
+                this.image1Field = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute(Namespace = "http://www.itunes.com/dtds/podcast-1.0.dtd")]
+        public category category {
+            get {
+                return this.categoryField;
+            }
+            set {
+                this.categoryField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute("item")]
+        public rssChannelItem[] item {
+            get {
+                return this.itemField;
+            }
+            set {
+                this.itemField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    public partial class rssChannelImage
+    {
+
+        private string urlField;
+
+        private string titleField;
+
+        private string linkField;
+
+        /// <remarks/>
+        public string url {
+            get {
+                return this.urlField;
+            }
+            set {
+                this.urlField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string title {
+            get {
+                return this.titleField;
+            }
+            set {
+                this.titleField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string link {
+            get {
+                return this.linkField;
+            }
+            set {
+                this.linkField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true, Namespace = "http://www.itunes.com/dtds/podcast-1.0.dtd")]
+    [System.Xml.Serialization.XmlRootAttribute(Namespace = "http://www.itunes.com/dtds/podcast-1.0.dtd", IsNullable = false)]
+    public partial class owner
+    {
+
+        private string nameField;
+
+        private string emailField;
+
+        /// <remarks/>
+        public string name {
+            get {
+                return this.nameField;
+            }
+            set {
+                this.nameField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string email {
+            get {
+                return this.emailField;
+            }
+            set {
+                this.emailField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true, Namespace = "http://www.itunes.com/dtds/podcast-1.0.dtd")]
+    [System.Xml.Serialization.XmlRootAttribute(Namespace = "http://www.itunes.com/dtds/podcast-1.0.dtd", IsNullable = false)]
+    public partial class image
+    {
+
+        private string hrefField;
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public string href {
+            get {
+                return this.hrefField;
+            }
+            set {
+                this.hrefField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true, Namespace = "http://www.itunes.com/dtds/podcast-1.0.dtd")]
+    [System.Xml.Serialization.XmlRootAttribute(Namespace = "http://www.itunes.com/dtds/podcast-1.0.dtd", IsNullable = false)]
+    public partial class category
+    {
+
+        private string textField;
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public string text {
+            get {
+                return this.textField;
+            }
+            set {
+                this.textField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    public partial class rssChannelItem
+    {
+
+        private string titleField;
+
+        private string linkField;
+
+        private string descriptionField;
+
+        private string categoryField;
+
+        private string pubDateField;
+
+        private string guidField;
+
+        private rssChannelItemEnclosure enclosureField;
+
+        private string durationField;
+
+        /// <remarks/>
+        public string title {
+            get {
+                return this.titleField;
+            }
+            set {
+                this.titleField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string link {
+            get {
+                return this.linkField;
+            }
+            set {
+                this.linkField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string description {
+            get {
+                return this.descriptionField;
+            }
+            set {
+                this.descriptionField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string category {
+            get {
+                return this.categoryField;
+            }
+            set {
+                this.categoryField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string pubDate {
+            get {
+                return this.pubDateField;
+            }
+            set {
+                this.pubDateField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string guid {
+            get {
+                return this.guidField;
+            }
+            set {
+                this.guidField = value;
+            }
+        }
+
+        /// <remarks/>
+        public rssChannelItemEnclosure enclosure {
+            get {
+                return this.enclosureField;
+            }
+            set {
+                this.enclosureField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute(Namespace = "http://www.itunes.com/dtds/podcast-1.0.dtd")]
+        public string duration {
+            get {
+                return this.durationField;
+            }
+            set {
+                this.durationField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    public partial class rssChannelItemEnclosure
+    {
+
+        private string urlField;
+
+        private uint lengthField;
+
+        private string typeField;
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public string url {
+            get {
+                return this.urlField;
+            }
+            set {
+                this.urlField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public uint length {
+            get {
+                return this.lengthField;
+            }
+            set {
+                this.lengthField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public string type {
+            get {
+                return this.typeField;
+            }
+            set {
+                this.typeField = value;
+            }
+        }
+    }
+
+
+}
